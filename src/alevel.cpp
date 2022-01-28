6e5eaa41ef85a220ea6d73b5502cad46ef28464d
Will <twitchifygamers@gmail.com>
2022-01-28 17:43:05 +0000

    ASLevels are done

--- src/alevel.cpp
+++ src/alevel.cpp
@@ -128,3 +128,31 @@ int ALevelDoubleAward(std::string pts)
         return -1;
     }
 }
+
+int ASLevel(std::string pts)
+{
+    if (pts == "A")
+    {
+        return 20;
+    }
+    else if (pts == "B")
+    {
+        return 16;
+    }
+    else if (pts == "C")
+    {
+        return 12;
+    }
+    else if (pts == "D")
+    {
+        return 10;
+    }
+    else if (pts == "E")
+    {
+        return 6;
+    }
+    else
+    {
+        return -1;
+    }
+}
