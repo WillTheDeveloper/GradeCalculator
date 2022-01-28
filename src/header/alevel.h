e66fed74143c861a74c6f638bad0599cfb43921a
Will <twitchifygamers@gmail.com>
2022-01-28 18:03:54 +0000

    Documented all aLEvels

--- src/header/alevel.h
+++ src/header/alevel.h
@@ -9,10 +9,23 @@
  */
 
 namespace ALevel {
+    // Valid grades: A*, A, B, C, D, E (= 56, 16, 12, 10, 6)
     int ALevel(std::string pts);
+
+    // Valid grades: A*A, AA, AB, BB, BC, CC, CD, DD, DE, EE
+    // (= 76, 68, 64, 56, 52, 44, 42, 34, 30, 22)
     int ALevelNineUnitAward(std::string pts);
+
+    // Valid grades: A*A, AA, AB, BB, BC, CC, CD, DD, DE, EE
+    // (= 112, 104, 96, 80, 72, 64, 56, 48, 40, 32)
     int ALevelDoubleAward(std::string pts);
+
+    // Valid grades: A, B, C, D, E
+    // (= 20, 16, 12, 10, 6)
     int ASLevel(std::string pts);
+
+    // Valid grades: AA, AB, BB, BC, CC, CD, DD, DE, EE
+    // (= 40, 36, 32, 28, 24, 22, 20, 16, 12
     int ASLevelDoubleAward(std::string pts);
 }
 
