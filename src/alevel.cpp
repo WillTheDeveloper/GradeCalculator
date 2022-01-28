46aa063c25da9700650aa9dd6e300a3a641dbe8c
Will <twitchifygamers@gmail.com>
2022-01-28 17:37:21 +0000

    Finished adding all ALevelNineUnitAward

--- src/alevel.cpp
+++ src/alevel.cpp
@@ -35,5 +35,48 @@ int ALevel(std::string pts)
 
 int ALevelNineUnitAward(std::string pts)
 {
-
+    if (pts == "A*A")
+    {
+        return 76;
+    }
+    else if (pts == "AA")
+    {
+        return 68;
+    }
+    else if (pts == "AB")
+    {
+        return 64;
+    }
+    else if (pts == "BB")
+    {
+        return 56;
+    }
+    else if (pts == "BC")
+    {
+        return 52;
+    }
+    else if (pts == "CC")
+    {
+        return 44;
+    }
+    else if (pts == "CD")
+    {
+        return 42;
+    }
+    else if (pts == "DD")
+    {
+        return 34;
+    }
+    else if (pts == "DE")
+    {
+        return 30;
+    }
+    else if (pts == "EE")
+    {
+        return 22;
+    }
+    else
+    {
+        return -1;
+    }
 }
