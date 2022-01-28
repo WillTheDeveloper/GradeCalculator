e4078e352e94f211b0d5fcca8faf62de6374eaf0
colsak <colsak@outlook.com>
2022-01-28 17:52:24 +0000

    Finished BTEC headers

--- src/header/btecnqf.h
+++ src/header/btecnqf.h
@@ -1,6 +1,19 @@
 #ifndef BTEC_NQF
 #define BTEC_NQF
+#include <string>
 
+/* BTEC National (NQF) qualification library
+ *
+ * On Exception/Error, the function will return a -1 (int).
+ *
+ *
+ */
+
+namespace BTECNationalNQF {
+    int NationalAward(std::string grade);
+    int NationalCertificate(std::string grade);
+    int NationalDiploma(std::string grade);
+}
 
 
 #endif
