9ec8a7c9afbbe9a2eb4dc6b4f6e52c2e36488ab3
Will <twitchifygamers@gmail.com>
2022-01-28 17:30:47 +0000

    Added first aLevel

--- src/alevel.cpp
+++ src/alevel.cpp
@@ -3,13 +3,37 @@
 
 int ALevel(std::string pts)
 {
-    switch (pts) {
-        case "A*": return 56;
-        case "A": return 48;
-        case "B": return 40;
-        case "C": return 32;
-        case "D": return 24;
-        case "E": return 16;
-        default: return -1;
+    if (pts == "A*")
+    {
+        return 56;
     }
+    else if (pts == "A")
+    {
+        return 48;
+    }
+    else if (pts == "B")
+    {
+        return 40;
+    }
+    else if (pts == "C")
+    {
+        return 32;
+    }
+    else if (pts == "D")
+    {
+        return 24;
+    }
+    else if (pts == "E")
+    {
+        return 16;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int ALevelNineUnitAward(std::string pts)
+{
+
 }
