92ec18455a473b01ca81cb708c0f027c327d8fd3
Will <twitchifygamers@gmail.com>
2022-01-28 17:25:08 +0000

    Added first aLevel

--- src/alevel.cpp
+++ src/alevel.cpp
@@ -1,1 +1,15 @@
 #include "header/alevel.h"
+#include "string"
+
+int ALevel(std::string pts)
+{
+    switch (pts) {
+        case "A*": return 56;
+        case "A": return 48;
+        case "B": return 40;
+        case "C": return 32;
+        case "D": return 24;
+        case "E": return 16;
+        default: return -1;
+    }
+}
