c22ad8f00c9d816ce2c71511136b4773f6068550
Will <twitchifygamers@gmail.com>
2022-01-29 16:47:29 +0000

    Created simple ALevel test

--- tests/alevel.cpp
+++ tests/alevel.cpp
@@ -1,4 +1,25 @@
 //
 // Created by will on 28/01/2022.
 //
+#include "../src/header/alevel.h"
+#include <iostream>
 
+using namespace ALevel;
+
+bool TestALevel()
+{
+    int expects = 56;
+
+    std::cout << "Testing" << std::endl;
+    int value = ALevel("A*");
+    std::cout << "Expects: " << expects << std::endl;
+    std::cout << "Actual: " << value;
+
+    if (value == expects)
+    {
+        return true
+    } else
+    {
+        return false
+    }
+}
