5f52b2a8b47d5196e4b576aa83a17f826ce5bbf5
William Burton <twitchifygamers@gmail.com>
2022-01-31 12:13:06 +0000

    Added structure to technical 1080

--- src/aqatechnical.cpp
+++ src/aqatechnical.cpp
@@ -30,7 +30,50 @@ int L3FoundationTechnical(std::string pts)
 
 int L3Technical1080(std::string pts)
 {
+    if(pts == "D*D*D*")
+    {
 
+    }
+    else if(pts == "D*D*D")
+    {
+
+    }
+    else if(pts == "D*DD")
+    {
+
+    }
+    else if(pts == "DDD")
+    {
+
+    }
+    else if(pts == "DDM")
+    {
+
+    }
+    else if(pts == "DMM")
+    {
+
+    }
+    else if(pts == "MMM")
+    {
+
+    }
+    else if(pts == "MMP")
+    {
+
+    }
+    else if(pts == "MPP")
+    {
+
+    }
+    else if(pts == "PPP")
+    {
+
+    }
+    else
+    {
+        return -1;
+    }
 }
 
 int L3Technical540(std::string pts)
