3bda211a6ede418979796ab4071f6a0ca0e714bb
William Burton <twitchifygamers@gmail.com>
2022-01-31 15:03:01 +0000

    Added return values to some aqa tech

--- src/aqatechnical.cpp
+++ src/aqatechnical.cpp
@@ -8,19 +8,19 @@ int L3FoundationTechnical(std::string pts)
 {
     if(pts == "D*")
     {
-
+        return 56;
     }
     else if(pts == "D")
     {
-
+        return 48;
     }
     else if(pts == "M")
     {
-
+        return 32;
     }
     else if(pts == "P")
     {
-
+        return 16;
     }
     else
     {
@@ -32,43 +32,43 @@ int L3Technical1080(std::string pts)
 {
     if(pts == "D*D*D*")
     {
-
+        return 168;
     }
     else if(pts == "D*D*D")
     {
-
+        return 160;
     }
     else if(pts == "D*DD")
     {
-
+        return 152;
     }
     else if(pts == "DDD")
     {
-
+        return 144;
     }
     else if(pts == "DDM")
     {
-
+        return 128;
     }
     else if(pts == "DMM")
     {
-
+        return 112;
     }
     else if(pts == "MMM")
     {
-
+        return 96;
     }
     else if(pts == "MMP")
     {
-
+        return 80;
     }
     else if(pts == "MPP")
     {
-
+        return 64;
     }
     else if(pts == "PPP")
     {
-
+        return 48;
     }
     else
     {
