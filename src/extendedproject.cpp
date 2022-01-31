ca8debbdeac9c9d9b2bf0f2b2fbc4ad62711b802
William Burton <twitchifygamers@gmail.com>
2022-01-31 11:53:01 +0000

    Added extendedproject grade points

---
+++ src/extendedproject.cpp
@@ -0,0 +1,35 @@
+#include "header/extendedproject.h"
+#include <iostream>
+#include "string"
+
+int ExtendedProject(std::string pts)
+{
+    if (pts == "A*")
+    {
+        return 28;
+    }
+    else if (pts == "A")
+    {
+        return 24;
+    }
+    else if (pts == "B")
+    {
+        return 20;
+    }
+    else if (pts == "C")
+    {
+        return 16;
+    }
+    else if (pts == "D")
+    {
+        return 12;
+    }
+    else if (pts == "E")
+    {
+        return 8;
+    }
+    else
+    {
+        return -1;
+    }
+}
