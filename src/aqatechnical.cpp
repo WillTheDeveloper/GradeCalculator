86def1b5cf5d47407e6dd74a541e220a7506b604
William Burton <twitchifygamers@gmail.com>
2022-01-31 12:08:05 +0000

    Started constructing aqatechnical files

---
+++ src/aqatechnical.cpp
@@ -0,0 +1,22 @@
+#include "string"
+#include <iostream>
+#include "header/aqatechnical.h"
+
+using namespace AQATechnicalN;
+
+int L3FoundationTechnical(std::string pts)
+{
+
+}
+
+int L3Technical1080(std::string pts)
+{
+
+}
+
+int L3Technical540(std::string pts)
+{
+
+}
+
+int L3Technical720(std::string pts)
